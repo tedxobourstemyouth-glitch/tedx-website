0aa9c5533717ee4f2481377ba004fa0e7e2f8540
TEDx Obour STEM Youth <tedxobourstemyouth@gmail.com>
2026-06-25 01:56:25 +0300

    Update payment section UI

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-﻿﻿document.addEventListener('DOMContentLoaded', () => {
+﻿document.addEventListener('DOMContentLoaded', () => {
   // --- Preloader ---
   const preloader = document.getElementById('preloader');
   if (preloader) {
